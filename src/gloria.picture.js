c11a210c724421d891796626c06867aa70183dde
Prince WAMINA <princewamina@gmail.com>
2021-06-18 23:14:49 +0100

    First Version

--- src/gloria.picture.js
+++ src/gloria.picture.js
@@ -1,39 +1,62 @@
 'use strict';
 var GloriaJS = GloriaJS || {};
 
+/**
+ * 
+ */
 GloriaJS.pictures = (()=>{
     
     let picturesOnPage;
     
+    /** 
+     * 
+     */
     document.addEventListener("DOMContentLoaded",()=>{
+
+        //List of items with gl-picture class
         picturesOnPage = document.getElementsByClassName("gl-picture");
         
+        // Looping for setting things on for each item found with gl-picture class
         for (const glPicture of picturesOnPage) {
 
+            // If the title property is not set, then GlPicture will set it 
             if(glPicture.getAttribute("title")==""){
-                glPicture.setAttribute("title","Cliquer pour modifier l'image...");
+                glPicture.setAttribute("title","Click to select a picture...");
             }
 
+            // By default, all gl-picture items have the pointer instead of the arrow cursor
             glPicture.style.cursor = "pointer";
 
+            // Now creatin the file selector (<input type="file">)
             const fileViewer = document.createElement("input");
             fileViewer.type = "file";
+            // Contraint to accept only Pictures
             fileViewer.accept = "image/*";
+            // Hidding our selector
             fileViewer.setAttribute("hidden","hidden");
 
-           
+            // Adding the selector in the body of our html document
             document.body.appendChild(fileViewer);
 
+            // Hidden field for data manipulations purpose
             let hiddenValue = document.createElement("input");
             if(document.getElementById(glPicture.dataset.glSave)!=undefined){
                 hiddenValue.type = "hidden";
+                // Each hidden field will have its name from a glPicture element
                 hiddenValue.name = glPicture.getAttribute("name");
                 document.getElementById(glPicture.dataset.glSave).appendChild(hiddenValue)
             }
 
+            // Protecting pictures from downloading
             if(glPicture.dataset.glProtect!=undefined){
+
+                // checking if data-gl-protect = "true" or data-gl-protect = "protect"
                 if(glPicture.dataset.glProtect.toLowerCase() == "true" || glPicture.dataset.glProtect.toLowerCase() == "protect"){
+                    
+                    // Forcing the cursor to be the arrow one
                     glPicture.style.cursor = "default";
+
+                    // The protection is a transparent div with the concerned GlPicture size
                     const protection = document.createElement("div");
                     protection.style.width = glPicture.style.width;
                     if(glPicture.getAttribute("width")!=undefined){
@@ -57,14 +80,17 @@ GloriaJS.pictures = (()=>{
                         protection.style.marginLeft = "-"+protection.style.width;
                     }
                     
+                    // Adding the protection
                     glPicture.after(protection);
                 }
             }
 
+            // Linking the GlPicture item click with its fileviewer click
             glPicture.addEventListener("click",()=>{
                 fileViewer.click();
             });
 
+            // Hadling the fileViewer change event
             fileViewer.addEventListener("change",(e)=>{
                 let glStreamer = new FileReader();
                 glStreamer.onload = function(evt){
@@ -105,7 +131,6 @@ GloriaJS.pictures = (()=>{
        
     });
 
-    console.log("Gloria Pictures By Prince WAMINA");
+    console.log("Gloria Pictures By T. Prince WAMINA");
 
 })();
-
